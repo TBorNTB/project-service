7a7a1eaa85bead646fa0358e8727ae71176c23b9
PlusUltraCode <kkd06144@naver.com>
2025-07-05 16:08:45 +0900

    #1 feat : 프로젝트 카테고리 및 상태 Enum 정비

---
+++ src/main/java/com/sejong/projectservice/core/enums/Category.java
@@ -0,0 +1,11 @@
+package com.sejong.projectservice.core.enums;
+
+public enum Category {
+    SYSTEM_HACKING,
+    WEB_HACKING,
+    DIGITAL_FORENSICS,
+    REVERSING,
+    CRYPTOGRAPHY,
+    NETWORK_SECURITY,
+    IOT_SECURITY
+}
