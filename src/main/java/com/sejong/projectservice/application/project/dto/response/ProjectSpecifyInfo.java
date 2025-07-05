5a785bace5a4e44b7392c3ffebac7b73491bde2b
PlusUltraCode <kkd06144@naver.com>
2025-07-05 16:06:11 +0900

    #1 feat: 프로젝트 요청 및 응답 DTO 구조 변경

---
+++ src/main/java/com/sejong/projectservice/application/project/dto/response/ProjectSpecifyInfo.java
@@ -0,0 +1,52 @@
+package com.sejong.projectservice.application.project.dto.response;
+
+import com.sejong.projectservice.core.collaborator.Collaborator;
+import com.sejong.projectservice.core.enums.Category;
+import com.sejong.projectservice.core.enums.ProjectStatus;
+import com.sejong.projectservice.core.project.domain.Project;
+import com.sejong.projectservice.core.subgoal.SubGoal;
+import com.sejong.projectservice.core.techstack.TechStack;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ProjectSpecifyInfo {
+    private Long id;
+    private String title;
+    private String description;
+    private Category category;
+    private ProjectStatus projectStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String thumbnailUrl;
+    private String contentJson;
+    private List<SubGoal> subGoals;
+    private List<TechStack> techStacks = new ArrayList<>();
+    private List<Collaborator> collaborators = new ArrayList<>();
+
+    public static ProjectSpecifyInfo from(Project project) {
+        return ProjectSpecifyInfo.builder()
+                .id(project.getId())
+                .title(project.getTitle())
+                .description(project.getDescription())
+                .category(project.getCategory())
+                .projectStatus(project.getProjectStatus())
+                .createdAt(project.getCreatedAt())
+                .updatedAt(project.getUpdatedAt())
+                .thumbnailUrl(project.getThumbnailUrl())
+                .contentJson(project.getContentJson())
+                .subGoals(project.getSubGoals())
+                .techStacks(project.getTechStacks())
+                .collaborators(project.getCollaborators())
+                .build();
+    }
+}
