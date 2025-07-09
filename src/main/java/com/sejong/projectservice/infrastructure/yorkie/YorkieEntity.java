4066593e5c4d9eeabc7b5cc8127e5da0676d9d1f
PlusUltraCode <kkd06144@naver.com>
2025-07-09 09:35:59 +0900

    #3 feat(yorkie): yorkie 도메인, 엔티티, JPA 리포지토리 구현

--- src/main/java/com/sejong/projectservice/infrastructure/yorkie/YorkieEntity.java
+++ src/main/java/com/sejong/projectservice/infrastructure/yorkie/YorkieEntity.java
@@ -1,4 +1,43 @@
 package com.sejong.projectservice.infrastructure.yorkie;
 
+import com.sejong.projectservice.core.yorkie.Yorkie;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "yorkie",
+        indexes = {
+                @Index(name = "uk_project_id", columnList = "projectId", unique = true)
+        })
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class YorkieEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long projectId;
+    private Long yorkieId; //일단은 yorkieId가 바뀌지 않는다는 가정
+
+    public static YorkieEntity from(Yorkie yorkie) {
+        return YorkieEntity.builder()
+                .id(null)
+                .projectId(yorkie.getProjectId())
+                .yorkieId(yorkie.getYorkieId())
+                .build();
+    }
+
+    public Yorkie toDomain() {
+        return Yorkie.builder()
+                .projectId(this.projectId)
+                .yorkieId(this.yorkieId)
+                .build();
+    }
 }
--- src/main/java/com/sejong/projectservice/infrastructure/yorkie/repository/YorkieJpaRepository.java
+++ src/main/java/com/sejong/projectservice/infrastructure/yorkie/repository/YorkieJpaRepository.java
@@ -1,4 +1,14 @@
 package com.sejong.projectservice.infrastructure.yorkie.repository;
 
-public class YorkieJpaRepository {
+import com.sejong.projectservice.infrastructure.yorkie.YorkieEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface YorkieJpaRepository extends JpaRepository<YorkieEntity, Long> {
+
+    @Query("select y.yorkieId from YorkieEntity y where y.projectId = :projectId ")
+    Optional<Long> findByProjectId(@Param("projectId")Long projectId);
 }
