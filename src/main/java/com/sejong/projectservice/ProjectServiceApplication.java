b844f933365c9bacc148e778e614e0062a07f9ad
PlusUltraCode <kkd06144@naver.com>
2025-07-09 09:36:02 +0900

    #3 chore: 프로젝트 설정 및 project 도메인 보완

--- src/main/java/com/sejong/projectservice/ProjectServiceApplication.java
+++ src/main/java/com/sejong/projectservice/ProjectServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 //@EnableFeignClients(basePackages = "com.sejong.projectservice.infrastructure.client")
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 public class ProjectServiceApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/sejong/projectservice/core/project/domain/Project.java
+++ src/main/java/com/sejong/projectservice/core/project/domain/Project.java
@@ -30,6 +30,7 @@ public class Project {
     private String thumbnailUrl;
     private String contentJson;
     private Long userId;
+    private Long yorkieId;
     private List<SubGoal> subGoals =new ArrayList<>();
     private List<TechStack> techStacks = new ArrayList<>();
     private List<Collaborator> collaborators = new ArrayList<>();
