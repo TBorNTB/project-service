bc3ce4d601785fde8350302cbc4faa32510432dc
PlusUltraCode <kkd06144@naver.com>
2025-07-20 15:13:07 +0900

    chore : cicd 배포 테스트 1

--- src/main/java/com/sejong/projectservice/ProjectServiceApplication.java
+++ src/main/java/com/sejong/projectservice/ProjectServiceApplication.java
@@ -2,8 +2,7 @@ package com.sejong.projectservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
+
 
 @SpringBootApplication
 //@EnableFeignClients(basePackages = "com.sejong.projectservice.infrastructure.client")
--- src/main/java/com/sejong/projectservice/infrastructure/client/UserClient.java
+++ src/main/java/com/sejong/projectservice/infrastructure/client/UserClient.java
@@ -1,8 +1,5 @@
 package com.sejong.projectservice.infrastructure.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "user-service")
 //public interface UserClient {
