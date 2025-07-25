03ba0d909e42b27e2a4257a271fde92ee7d0c503
PlusUltraCode <kkd06144@naver.com>
2025-07-25 17:44:59 +0900

    #7 fix: ProjectPageResponse Command 기반 구조에 맞게 수정

--- src/main/java/com/sejong/projectservice/application/project/dto/response/ProjectPageResponse.java
+++ src/main/java/com/sejong/projectservice/application/project/dto/response/ProjectPageResponse.java
@@ -1,11 +1,11 @@
 package com.sejong.projectservice.application.project.dto.response;
 
+import com.sejong.projectservice.core.common.PageResult;
 import com.sejong.projectservice.core.project.domain.Project;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -20,9 +20,9 @@ public class ProjectPageResponse {
     Long totalElements;
     int page;
 
-    public static ProjectPageResponse from(Page<Project> projectPage) {
+    public static ProjectPageResponse from(PageResult<Project> projectPage) {
 
-        List<ProjectSimpleInfo> projectSimpleInfos = projectPage.stream()
+        List<ProjectSimpleInfo> projectSimpleInfos = projectPage.getContent().stream()
                 .map(ProjectSimpleInfo::from)
                 .toList();
 
