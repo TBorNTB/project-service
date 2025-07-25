29edd23e619a669b0e895488c42c80a23150d2bc
PlusUltraCode <kkd06144@naver.com>
2025-07-25 17:44:38 +0900

    #7 feat: ProjectFormRequest → ProjectFormCommand 매핑 구현

--- src/main/java/com/sejong/projectservice/core/project/command/ProjectFormCommand.java
+++ src/main/java/com/sejong/projectservice/core/project/command/ProjectFormCommand.java
@@ -1,4 +1,23 @@
 package com.sejong.projectservice.core.project.command;
 
+import com.sejong.projectservice.core.enums.Category;
+import com.sejong.projectservice.core.enums.ProjectStatus;
+import lombok.Builder;
+import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Builder
+@Data
 public class ProjectFormCommand {
+    private String title;
+    private LocalDateTime createdAt;
+    private String description;
+    private Category category;
+    private ProjectStatus projectStatus;
+    private List<String> collaborators;
+    private List<String> techStacks;
+    private List<String> subGoals;
+    private String thumbnail;
+    private String contentJson;
 }
