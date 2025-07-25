6640298199a48af9e5139f1c5a24e16f0d75b2f9
PlusUltraCode <kkd06144@naver.com>
2025-07-25 17:43:10 +0900

    #7 feat: ProjectFormRequest를 Command로 변환하는 매퍼 클래스 추가

--- src/main/java/com/sejong/projectservice/application/mapper/ProjectCommandMapper.java
+++ src/main/java/com/sejong/projectservice/application/mapper/ProjectCommandMapper.java
@@ -1,4 +1,22 @@
 package com.sejong.projectservice.application.mapper;
 
+import com.sejong.projectservice.application.project.dto.request.ProjectFormRequest;
+import com.sejong.projectservice.core.project.command.ProjectFormCommand;
+
 public class ProjectCommandMapper {
+
+    public static ProjectFormCommand toCommand(ProjectFormRequest request) {
+        return ProjectFormCommand.builder()
+                .title(request.getTitle())
+                .createdAt(request.getCreatedAt())
+                .description(request.getDescription())
+                .category(request.getCategory())
+                .projectStatus(request.getProjectStatus())
+                .collaborators(request.getCollaborators())
+                .techStacks(request.getTechStacks())
+                .subGoals(request.getSubGoals())
+                .thumbnail(request.getThumbnail())
+                .contentJson(request.getContentJson())
+                .build();
+    }
 }
