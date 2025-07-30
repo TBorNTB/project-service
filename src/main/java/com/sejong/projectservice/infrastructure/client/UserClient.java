11b4c392a9d328ca7ef36b9794e334215c377556
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2025-07-30 14:36:39 +0900

    chore: 경로 수정 및 문서 수정

--- src/main/java/com/sejong/projectservice/infrastructure/client/UserClient.java
+++ src/main/java/com/sejong/projectservice/infrastructure/client/UserClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "user-service", path = "/users")
+@FeignClient(name = "user-service", path = "/internal")
 public interface UserClient {
     @GetMapping("/{userId}/exists")
     ResponseEntity<Boolean> exists(@PathVariable("userId") String userId);
