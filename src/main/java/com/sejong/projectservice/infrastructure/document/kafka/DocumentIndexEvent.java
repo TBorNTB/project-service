cac816406e4ec4e1c084b17a701fdb7b7d3d42c1
PlusUltraCode <kkd06144@naver.com>
2025-09-03 16:53:35 +0900

    chore: ProjectDocument -> ProjectEvent 네이밍 변경

--- src/main/java/com/sejong/projectservice/infrastructure/document/kafka/DocumentIndexEvent.java
+++ src/main/java/com/sejong/projectservice/infrastructure/document/kafka/DocumentIndexEvent.java
@@ -2,15 +2,11 @@ package com.sejong.projectservice.infrastructure.document.kafka;
 
 
 import com.sejong.projectservice.core.document.domain.Document;
-import com.sejong.projectservice.core.project.domain.Project;
-import com.sejong.projectservice.infrastructure.project.kafka.ProjectDocument;
 import com.sejong.projectservice.infrastructure.project.kafka.enums.Type;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.print.Doc;
 
 @Data
 @AllArgsConstructor
@@ -20,15 +16,15 @@ public class DocumentIndexEvent {
     private String aggregatedId;
     private Type type;
     private long occurredAt;
-    private DocumentDocument documentDocument;
+    private DocumentEvent documentEvent;
 
     public static DocumentIndexEvent of(Document document, Type type, long occurredAt) {
-        DocumentDocument documentDocument = DocumentDocument.from(document);
+        DocumentEvent documentEvent = DocumentEvent.from(document);
         return DocumentIndexEvent.builder()
-                .aggregatedId(documentDocument.getId())
+                .aggregatedId(documentEvent.getId())
                 .type(type)
                 .occurredAt(occurredAt)
-                .documentDocument(documentDocument)
+                .documentEvent(documentEvent)
                 .build();
     }
 
