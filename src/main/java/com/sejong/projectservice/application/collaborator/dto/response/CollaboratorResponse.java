b42560b47cd1f9759d27721d5cc72d1afa7db797
PlusUltraCode <kkd06144@naver.com>
2025-09-04 15:15:45 +0900

    #22 feat: ProjectUsernamesExtractor, CollaboratorResponse 신규 추가

---
+++ src/main/java/com/sejong/projectservice/application/collaborator/dto/response/CollaboratorResponse.java
@@ -0,0 +1,4 @@
+package com.sejong.projectservice.application.collaborator.dto.response;
+
+public class CollaboratorResponse {
+}
---
+++ src/main/java/com/sejong/projectservice/application/project/service/ProjectUsernamesExtractor.java
@@ -0,0 +1,4 @@
+package com.sejong.projectservice.application.project.service;
+
+public class ProjectUsernamesExtractor {
+}
