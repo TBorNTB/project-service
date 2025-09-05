47190af0d288d2c54e5f308bf2d2ef5d98a7f8e2
PlusUltraCode <kkd06144@naver.com>
2025-09-05 14:42:57 +0900

    feat(client) : userId -> username 변경

--- src/main/java/com/sejong/projectservice/infrastructure/client/UserClient.java
+++ src/main/java/com/sejong/projectservice/infrastructure/client/UserClient.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-service", path = "/internal")
 public interface UserClient {
-    @GetMapping("/{userId}/exists")
-    ResponseEntity<Boolean> exists(@PathVariable("userId") String userId);
+    @GetMapping("/{username}/exists")
+    ResponseEntity<Boolean> exists(@PathVariable("username") String username);
 
     @GetMapping("/exists")
     ResponseEntity<Boolean> existAll(@RequestBody List<String> nicknames);
