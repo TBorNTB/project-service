283c3d961dc01e110961043c72290ba4420c2f8b
PlusUltraCode <kkd06144@naver.com>
2025-12-18 14:36:39 +0900

    chore(config): Swagger 및 애플리케이션 설정 정리

--- src/main/java/com/sejong/projectservice/application/common/config/SwaggerConfig.java
+++ src/main/java/com/sejong/projectservice/application/common/config/SwaggerConfig.java
@@ -3,6 +3,10 @@ package com.sejong.projectservice.application.common.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -18,4 +22,16 @@ import org.springframework.context.annotation.Configuration;
 )
 @Configuration
 public class SwaggerConfig {
+        @Bean
+        public OpenAPI openAPI() {
+                return new OpenAPI()
+                        .components(new Components()
+                                .addSecuritySchemes("bearerAuth",
+                                        new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                        );
+        }
 }
