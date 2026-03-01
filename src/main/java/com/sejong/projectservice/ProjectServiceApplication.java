39b6d7581c88960cebccfb4c69cc85cbeadb619c
PlusUltraCode <kkd06144@naver.com>
2026-03-01 16:52:06 +0900

    세계시 -> 한국시로 변경

--- src/main/java/com/sejong/projectservice/ProjectServiceApplication.java
+++ src/main/java/com/sejong/projectservice/ProjectServiceApplication.java
@@ -1,11 +1,19 @@
 package com.sejong.projectservice;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 
 @SpringBootApplication
 public class ProjectServiceApplication {
+
+    @PostConstruct
+    public void init() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProjectServiceApplication.class, args);
