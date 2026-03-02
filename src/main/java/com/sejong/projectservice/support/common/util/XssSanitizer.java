3cc3aec35c7ed882df0e754bba38c5c247637ac5
PlusUltraCode <kkd06144@naver.com>
2026-03-02 12:18:54 +0900

    Xss 정책 체크박스 허용

--- src/main/java/com/sejong/projectservice/support/common/util/XssSanitizer.java
+++ src/main/java/com/sejong/projectservice/support/common/util/XssSanitizer.java
@@ -3,7 +3,6 @@ package com.sejong.projectservice.support.common.util;
 import org.owasp.html.AttributePolicy;
 import org.owasp.html.HtmlPolicyBuilder;
 import org.owasp.html.PolicyFactory;
-import org.owasp.html.Sanitizers;
 import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
@@ -12,7 +11,7 @@ import java.util.stream.Collectors;
 /**
  * XSS 방어용 유틸.
  * - 일반 텍스트: HTML 이스케이프 (title, description 등)
- * - 본문(HTML): OWASP 라이브러리 기본 정책 조합 + 체크리스트(input/label/data-type) 허용
+ * - 본문(HTML): 대부분의 안전한 HTML 허용, script/iframe/on* 등 위험 요소만 차단
  */
 public final class XssSanitizer {
 
@@ -20,20 +19,43 @@ public final class XssSanitizer {
     private static final AttributePolicy INPUT_TYPE_CHECKBOX_ONLY = (elementName, attributeName, value) ->
             "checkbox".equalsIgnoreCase(value) ? "checkbox" : null;
 
-    /** 체크리스트/태스크리스트용: input[type=checkbox], label, data-type 허용 (TipTap 등 에디터 호환) */
-    private static final PolicyFactory CHECKLIST_POLICY = new HtmlPolicyBuilder()
-            .allowElements("input", "label")
+    /**
+     * 본문용 HTML 정책: 넓은 허용, 위험한 것만 차단.
+     * - 허용: 포맷/블록/코드(pre,code)/테이블/링크/이미지/체크리스트/스타일/class,id,title,data-type
+     * - 차단: script, iframe, object, embed, form, on* 이벤트, javascript: 프로토콜 (OWASP 기본 동작)
+     */
+    private static final PolicyFactory RICH_TEXT_POLICY = new HtmlPolicyBuilder()
+            // 공통 포맷/블록 (p, div, h1~h6, ul, ol, li, blockquote, b, i, code, span, br 등)
+            .allowCommonInlineFormattingElements()
+            .allowCommonBlockElements()
+            // 링크/이미지 태그 + 코드 블록, 테이블, 시맨틱/기타 블록
+            .allowElements(
+                    "a", "img",
+                    "pre", "code", "hr",
+                    "table", "tr", "td", "th", "thead", "tbody", "tfoot", "col", "colgroup", "caption",
+                    "figure", "figcaption", "section", "article", "header", "footer", "nav", "main", "aside",
+                    "details", "summary", "mark", "kbd", "samp", "var",
+                    "input", "label")
+            // 링크/이미지: http, https, mailto만 허용 (javascript: 등 차단)
+            .allowStandardUrlProtocols()
+            .allowAttributes("href", "rel").onElements("a")
+            .requireRelNofollowOnLinks()
+            .allowAttributes("src", "alt", "width", "height").onElements("img")
+            // 테이블 속성
+            .allowAttributes("summary").onElements("table")
+            .allowAttributes("align", "valign", "colspan", "rowspan").onElements("table", "tr", "td", "th", "col", "colgroup")
+            // 체크리스트
             .allowAttributes("type").matching(INPUT_TYPE_CHECKBOX_ONLY).onElements("input")
             .allowAttributes("checked").onElements("input")
-            .allowAttributes("data-type").onElements("ul", "ol", "li", "label", "div", "span")
+            // 에디터용 data-type (taskList, taskItem 등)
+            .allowAttributes("data-type").onElements("ul", "ol", "li", "label", "div", "span", "p", "section", "article")
+            // 공통 속성 (에디터 클래스/스타일 호환)
+            .allowAttributes("class", "id", "title").globally()
+            // style 허용 (OWASP 기본: 색상/폰트 등만, expression/behavior 등 차단)
+            .allowStyling()
+            // 속성 없이도 태그 유지 (span, input 등)
+            .allowWithoutAttributes("span", "input", "a", "img")
             .toFactory();
-
-    /** 라이브러리 제공 정책 + 체크리스트 정책. */
-    private static final PolicyFactory RICH_TEXT_POLICY = Sanitizers.FORMATTING
-            .and(Sanitizers.BLOCKS)
-            .and(Sanitizers.LINKS)
-            .and(Sanitizers.IMAGES)
-            .and(CHECKLIST_POLICY);
 
     private XssSanitizer() {
     }
