c44ea7074b48ba4b07c78117c74de6ad180f4ea1
PlusUltraCode <kkd06144@naver.com>
2026-03-02 12:04:29 +0900

    Xss 정책 체크박스 허용

--- src/main/java/com/sejong/projectservice/support/common/util/XssSanitizer.java
+++ src/main/java/com/sejong/projectservice/support/common/util/XssSanitizer.java
@@ -1,5 +1,7 @@
 package com.sejong.projectservice.support.common.util;
 
+import org.owasp.html.AttributePolicy;
+import org.owasp.html.HtmlPolicyBuilder;
 import org.owasp.html.PolicyFactory;
 import org.owasp.html.Sanitizers;
 import org.springframework.web.util.HtmlUtils;
@@ -10,15 +12,28 @@ import java.util.stream.Collectors;
 /**
  * XSS 방어용 유틸.
  * - 일반 텍스트: HTML 이스케이프 (title, description 등)
- * - 본문(HTML): OWASP 라이브러리 기본 정책 조합 (FORMATTING, BLOCKS, LINKS, IMAGES)
+ * - 본문(HTML): OWASP 라이브러리 기본 정책 조합 + 체크리스트(input/label/data-type) 허용
  */
 public final class XssSanitizer {
 
-    /** 라이브러리 제공 정책 조합. 허용 요소/속성은 OWASP 쪽에서 관리. */
+    /** input type 속성: checkbox만 허용 (XSS 방지) */
+    private static final AttributePolicy INPUT_TYPE_CHECKBOX_ONLY = (elementName, attributeName, value) ->
+            "checkbox".equalsIgnoreCase(value) ? "checkbox" : null;
+
+    /** 체크리스트/태스크리스트용: input[type=checkbox], label, data-type 허용 (TipTap 등 에디터 호환) */
+    private static final PolicyFactory CHECKLIST_POLICY = new HtmlPolicyBuilder()
+            .allowElements("input", "label")
+            .allowAttributes("type").matching(INPUT_TYPE_CHECKBOX_ONLY).onElements("input")
+            .allowAttributes("checked").onElements("input")
+            .allowAttributes("data-type").onElements("ul", "ol", "li", "label", "div", "span")
+            .toFactory();
+
+    /** 라이브러리 제공 정책 + 체크리스트 정책. */
     private static final PolicyFactory RICH_TEXT_POLICY = Sanitizers.FORMATTING
             .and(Sanitizers.BLOCKS)
             .and(Sanitizers.LINKS)
-            .and(Sanitizers.IMAGES);
+            .and(Sanitizers.IMAGES)
+            .and(CHECKLIST_POLICY);
 
     private XssSanitizer() {
     }
