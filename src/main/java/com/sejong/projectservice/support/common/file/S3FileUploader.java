9755fbfe26ce27f6e302c3b6e8f743cc8f6deaf1
Sumi Jeong <125195487+sigmaith@users.noreply.github.com>
2026-05-08 13:39:15 +0900

    fix: SVG Presigned URL 업로드 403 오류 수정
    content-type을 서명 헤더에서 제거하여 image/svg+xml의 + 문자로 인한 Garage 서명 불일치 문제 해결

--- src/main/java/com/sejong/projectservice/support/common/file/S3FileUploader.java
+++ src/main/java/com/sejong/projectservice/support/common/file/S3FileUploader.java
@@ -105,7 +105,6 @@ public class S3FileUploader implements FileUploader {
         PutObjectRequest putObjectRequest = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
-                .contentType(contentType)
                 .build();
 
         PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
